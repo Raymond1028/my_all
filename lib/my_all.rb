a614be2f0d0fa5d8b0fecb0c9dbe36e8ce259bd8
Ray's Git <lancelot53.ra@gmail.com>
2021-12-10 15:20:15 -0500

    Completed

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
-require 'pry'
+require "pry"
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while collection.length > i
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  #   block_return_values
+  # end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
+  #binding.pry
 end
